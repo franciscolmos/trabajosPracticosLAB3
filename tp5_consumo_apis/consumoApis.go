5d7df4d6c2d5a8d913f22e52995b52dda4378ada
Francisco Olmos <francisco.olmos.ubp@gmail.com>
2020-06-12 18:10:25 -0300

    vendor

--- tp5_consumo_apis/consumoApis.go
+++ tp5_consumo_apis/consumoApis.go
@@ -7,7 +7,7 @@ import (
 func ConsumoApi(){
 	r := gin.Default()
 	r.GET("https://api.mercadolibre.com/sites/MLA", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{ "message": "pong" })
+		c.JSON(http.StatusOK, gin.H{ "message": "" })
 	})
 	r.Run(":3000")
 }
